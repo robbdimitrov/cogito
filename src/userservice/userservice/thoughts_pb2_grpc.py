a211f47f686575f3a2170c66f26f39123dfe65a2
Robert Dimitrov <robert_dimitrov@me.com>
2026-05-13 19:29:19 +0300

    fix: use relative imports in generated protobuf gRPC files

--- src/userservice/userservice/thoughts_pb2_grpc.py
+++ src/userservice/userservice/thoughts_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import thoughts_pb2 as thoughts__pb2
+from . import thoughts_pb2 as thoughts__pb2
 
 GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
